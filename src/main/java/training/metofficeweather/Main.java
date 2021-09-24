0c2789cfe05f4a9edaeef5f07a5c774cf9cafd35
poonamale <msteeheeful@gmail.com>
2021-09-24 17:02:59 +0100

    takes userinput and returns locationId

--- src/main/java/training/metofficeweather/Main.java
+++ src/main/java/training/metofficeweather/Main.java
@@ -6,23 +6,69 @@ import org.glassfish.jersey.jackson.JacksonFeature;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
-import java.lang.reflect.Field;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
-    private static ObjectCodec objectMapper;
 
     public static void main(String args[]) {
+
         Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
-        Root got = client.target("http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=99f12f43-b144-4279-8507-f612268f0789")
+        Root siteList = client.target("http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=99f12f43-b144-4279-8507-f612268f0789")
                 .request(MediaType.APPLICATION_JSON)
                 .get(Root.class);
 
-//        HashMap<String, Location> getMap = new HashMap<>();
+        Location firstLocation = siteList.getLocations().getListOfLocations().get(0);
+        ArrayList<Location> locationArray = siteList.getLocations().getListOfLocations();
 
+
+        int locationID = firstLocation.getId();
+        String newURL = "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/" + locationID + "?res=3hourly&key=8fae71ad-9d25-43e0-8653-4808e2c64b89";
+
+        //System.out.println(newURL);
 
         //Create instance of location class with all appropriate properties
-        System.out.println(got.locations.getMap());
+        System.out.println();
+
+        printBanner();
+
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+
+        executeCommand(command, siteList, locationArray);
+
     }
-}	
+    private static void printBanner() {
+        System.out.println("Welcome to the Weather App");
+        System.out.println();
+        System.out.println("Please enter your request:");
+        System.out.println(" List All - list all locations");
+        System.out.println(" List [Account] - weather data for specified location");
+        System.out.println(" Quit application");
+        System.out.println();
+    }
+
+
+    public static void executeCommand(String command, Root siteList, ArrayList<Location> locations) {
+        if (command.equalsIgnoreCase("quit")) {
+            System.out.println("Goodbye.");
+            System.exit(0);
+        }
+        if (command.equalsIgnoreCase("all")) {
+            System.out.println(siteList.getLocations().getMap().keySet());
+        }
+        else {
+            for (Location eachLocation : locations) {
+                String locationName = String.valueOf(eachLocation.getName());
+                if (command.equalsIgnoreCase(locationName)) {
+                    String locationId = String.valueOf(eachLocation.getId());
+                    System.out.println(locationId);
+                }
+
+            }
+
+        }
+    }
+}
+
+
