6af56d5a5cb905b87e225dcfee2a2a23f935f1e7
Amber Shand <amber.shand@aexp.com>
2021-09-28 13:57:21 +0100

    did the weather info page using css

--- src/main/java/training/metofficeweather/Rep.java
+++ src/main/java/training/metofficeweather/Rep.java
@@ -82,6 +82,70 @@ public class Rep {
     }
 
     public String getW() {
+        if (W.equals("0")) {
+            W = "Clear night";
+        } else if (W.equals("1")) {
+            W = "Sunny day";
+        } else if (W.equals("2")) {
+            W = "Partly cloudy (night)\n";
+        } else if (W.equals("3")) {
+            W = "Partly cloudy (day)";
+        } else if (W.equals("4")) {
+            W = "Not used";
+        } else if (W.equals("5")) {
+            W = "Mist";
+        } else if (W.equals("6")) {
+            W = "Fog";
+        } else if (W.equals("7")) {
+            W = "Cloudy";
+        } else if (W.equals("8")) {
+            W = "Overcast";
+        } else if (W.equals("9")) {
+            W = "Light rain shower (night)";
+        } else if (W.equals("10")) {
+            W = "Light rain shower (day)";
+        } else if (W.equals("11")) {
+            W = "Drizzle";
+        } else if (W.equals("12")) {
+            W = "Light rain";
+        } else if (W.equals("13")) {
+            W = "Heavy rain shower (night)";
+        } else if (W.equals("14")) {
+            W = "Heavy rain shower (day)";
+        } else if (W.equals("15")) {
+            W = "Heavy rain";
+        } else if (W.equals("16")) {
+            W = "Sleet shower (night)";
+        } else if (W.equals("17")) {
+            W = "Sleet shower (day)";
+        } else if (W.equals("18")) {
+            W = "Sleet";
+        } else if (W.equals("19")) {
+            W = "Hail shower (night)";
+        } else if (W.equals("20")) {
+            W = "Hail shower (day)";
+        } else if (W.equals("21")) {
+            W = "Hail";
+        } else if (W.equals("22")) {
+            W = "Light snow shower (night)";
+        } else if (W.equals("23")) {
+            W = "Light snow shower (day)";
+        } else if (W.equals("24")) {
+            W = "Light snow";
+        } else if (W.equals("25")) {
+            W = "Heavy snow shower (night)";
+        } else if (W.equals("26")) {
+            W = "Heavy snow shower (day)";
+        } else if (W.equals("27")) {
+            W = "Heavy snow";
+        } else if (W.equals("28")) {
+            W = "Thunder shower (night)";
+        } else if (W.equals("29")) {
+            W = "Thunder shower (day)";
+        } else if (W.equals("30")) {
+            W = "Thunder";
+        }
+
         return W;
     }
 
